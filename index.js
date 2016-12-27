47ccdb71b486295d2e8e2e8d6a8ecc123be04bf7
kartheeknagasuri <kartheek@Kartheeks-MacBook-Pro.local>
2016-12-27 11:02:48 +0530

    implemented ReceiveMessage

--- index.js
+++ index.js
@@ -158,3 +158,17 @@ function callSendAPI(messageData) {
         }
     });
 }
+
+
+app.get('/receiveMesage', function(req, res) {
+    console.log("Received Message" + req);
+    //  console.log(req.query['hub.mode'])
+    // if (req.query['hub.mode'] === 'subscribe' &&
+    //     req.query['hub.verify_token'] === "lilToken") {
+    //     console.log("Validating webhook");
+    //     res.status(200).send(req.query['hub.challenge']);
+    // } else {
+    //     console.error("Failed to Validate. Make sure the validation tokens match.");
+    //     res.sendStatus(403);
+    // }
+});
